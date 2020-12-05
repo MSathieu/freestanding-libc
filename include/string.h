50e395fcba8c9a4d2c054406b62acbfcf2ac8998
MSathieu <18145111+MSathieu@users.noreply.github.com>
2020-12-05 10:51:26 +0100

    Add strncpy

--- include/string.h
+++ include/string.h
@@ -6,6 +6,7 @@ void* memcpy(void* restrict, const void* restrict, size_t);
 void* memmove(void*, const void*, size_t);
 void* memset(void*, int, size_t);
 int strcmp(const char*, const char*);
-int strncmp(const char*, const char*, size_t);
 char* strcpy(char* restrict, const char* restrict);
+int strncmp(const char*, const char*, size_t);
+char* strncpy(char* restrict, const char* restrict, size_t);
 size_t strlen(const char*);
